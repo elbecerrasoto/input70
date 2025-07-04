6a3decf46b408eabf467491e474395fb9affed5f
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-07-04 13:03:26 -0700

    add bigBoxes

--- app.R
+++ app.R
@@ -3,13 +3,21 @@ library(shiny)
 simulateRow <- fluidRow(
   column(2),
   column(8),
-  column(2)
+  column(2), radioButtons("shock_type", "Choose the type of Demand Shock to inject:",
+    choiceNames = list(
+      "Add (millions of pesos)",
+      "Multiply by previous demand",
+      "Input a total demand"
+    ),
+    choiceValues = list("add", "multiply", "raw")
+  )
 )
 
 bigboxRow <- fluidRow(
-  column(2),
-  column(8),
-  column(2)
+  column(3, textOutput("gdp_state_box")),
+  column(3, textOutput("employment_state_box")),
+  column(3, textOutput("gdp_rest_box")),
+  column(3, textOutput("employment_rest_box")),
 )
 
 inputRow <- fluidRow(
