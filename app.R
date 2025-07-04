446b540e7afab1484f415ac3cde3faa33f3a2423
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-07-04 12:36:48 -0700

    refactor

--- app.R
+++ app.R
@@ -1,33 +1,39 @@
 library(shiny)
 
+simulate_row <- fluidRow(
+  column(2),
+  column(8),
+  column(2)
+)
+
+bigbox_row <- fluidRow(
+  column(2),
+  column(8),
+  column(2)
+)
+
+input_row <- fluidRow(
+  column(2),
+  column(8),
+  column(2)
+)
+
+download_row <- fluidRow(
+  column(2),
+  column(8),
+  column(2)
+)
+
+
+
 ui <- fluidPage(
   titlePanel("Simulator"),
-  fluidRow(
-    column(2),
-    column(8),
-    column(2)
-  ),
-  fluidRow(
-    column(2),
-    column(8),
-    column(2)
-  ),
-  fluidRow(
-    column(2),
-    column(8),
-    column(2)
-  ),
-  fluidRow(
-    column(2),
-    column(8),
-    column(2)
-  ),
-  fluidRow(
-    column(2),
-    column(8),
-    column(2)
-  ),
+  simulate_row,
+  bigbox_row,
+  input_row,
+  download_row
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
