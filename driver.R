6967adb50ecef3f3eff411d175ebc0fa64c0e29b
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-07-05 14:06:31 -0700

    multiplicative decomposition

---
+++ driver.R
@@ -0,0 +1,59 @@
+#!/usr/bin/Rscript
+
+library(tidyverse)
+source("simulator.R")
+
+N_REGION <- 35
+N_OUTER <- 35
+
+N_SECTORS <- N_REGION + N_OUTER
+
+STATES <- read_rds("data/mips_br.Rds")
+
+results <- get_ZABLGfx_multipliers(STATES[["sinaloa"]], N_SECTORS)
+A <- results$A |> tib2mat()
+L <- results$L |> tib2mat()
+
+n <- ncol(A)
+r <- N_REGION
+s <- r + 1
+
+# ------ Regionalize
+
+Arr <- A[1:r, 1:r]
+Ars <- A[1:r, s:n]
+
+Ass <- A[s:n, s:n]
+Asr <- A[s:n, 1:r]
+
+Irr <- diag(N_REGION)
+Iss <- diag(N_OUTER)
+
+Ors <- matrix(0, nrow = N_REGION, ncol = N_OUTER)
+Osr <- matrix(0, nrow = N_OUTER, ncol = N_REGION)
+
+Lrr <- solve(Irr - Arr)
+Lss <- solve(Iss - Ass)
+
+Srs <- Lrr %*% Ars
+Ssr <- Lss %*% Asr
+
+Frr <- solve(Irr - Srs %*% Ssr)
+Fss <- solve(Iss - Ssr %*% Srs)
+
+M1_rr_sr_col <- rbind(Lrr, Osr)
+M1_rs_ss_col <- rbind(Ors, Lss)
+M1 <- cbind(M1_rr_sr_col, M1_rs_ss_col)
+
+M2_rr_sr_col <- rbind(Irr, Ssr)
+M2_rs_ss_col <- rbind(Srs, Iss)
+M2 <- cbind(M2_rr_sr_col, M2_rs_ss_col)
+
+M3_rr_sr_col <- rbind(Frr, Osr)
+M3_rs_ss_col <- rbind(Ors, Fss)
+M3 <- cbind(M3_rr_sr_col, M3_rs_ss_col)
+
+L321 <- M3 %*% M2 %*% M1
+
+Ls_equal <- near(L321, L, tol = 1e4) |> all()
+stopifnot("Mutiplicative Decomposition Failed" = Ls_equal)
